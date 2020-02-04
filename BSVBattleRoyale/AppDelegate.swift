7c301945f8e41309bb1029c3f95598ecb51d511a
Michael Redig <mredig@gmail.com>
2020-02-04 16:44:31 -0600

    removed netcode from app delegate and moved to its own controller

--- BSVBattleRoyale/AppDelegate.swift
+++ BSVBattleRoyale/AppDelegate.swift
@@ -9,43 +9,9 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, WebSocketConnectionDelegate {
-    func onConnected(connection: WebSocketConnection) {
-        print("Connected")
-    }
-    
-    func onDisconnected(connection: WebSocketConnection, error: Error?) {
-        if let error = error {
-            print("Disconnected with error:\(error)")
-        } else {
-            print("Disconnected normally")
-        }
-    }
-    
-    func onError(connection: WebSocketConnection, error: Error) {
-        print("Connection error:\(error)")
-    }
-    
-    func onMessage(connection: WebSocketConnection, text: String) {
-        print("Text message: \(text)")
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            self.webSocketConnection.send(text: "ping")
-        }
-    }
-    
-    func onMessage(connection: WebSocketConnection, data: Data) {
-        print("Data message: \(data)")
-    }
-    
-    var webSocketConnection: WebSocketConnection!
- 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        webSocketConnection = WebSocketTaskConnection(url: URL(string: "ws://demos.kaazing.com/echo")!)
-        webSocketConnection.delegate = self
-        
-        webSocketConnection.connect()
-        
-        webSocketConnection.send(text: "ping")
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {       
         
         return true
     }
