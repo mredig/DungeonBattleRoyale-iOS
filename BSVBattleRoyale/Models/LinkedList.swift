dab113d27a10b4bda2d8cafdd82f068724ac424a
Michael Redig <mredig@gmail.com>
2020-02-04 22:16:17 -0600

    added linked list type and tests

---
+++ BSVBattleRoyale/Models/LinkedList.swift
@@ -0,0 +1,55 @@
+//
+//  LinkedList.swift
+//  BSVBattleRoyale
+//
+//  Created by Michael Redig on 2/4/20.
+//  Copyright © 2020 joshua kaunert. All rights reserved.
+//
+
+import Foundation
+
+class LinkedList<T> {
+	class Node<T> {
+		let value: T
+		var next: Node<T>?
+		var previous: Node<T>?
+
+		init(value: T, next: Node?, previous: Node?) {
+			self.value = value
+			self.next = next
+			self.previous = previous
+		}
+
+		func insertBefore(value: T) {
+			let newNode = Node(value: value, next: self, previous: previous)
+			previous = newNode
+		}
+
+		func insertAfter(value: T) {
+			let newNode = Node(value: value, next: next, previous: self)
+			next = newNode
+		}
+	}
+
+	var head: Node<T>?
+	var tail: Node<T>?
+	var count: Int = 0
+
+	func addToHead(value: T) {
+		let newNode = Node(value: value, next: head, previous: nil)
+		head?.previous = newNode
+		head = newNode
+		if tail == nil {
+			tail = newNode
+		}
+	}
+
+	func addToTail(value: T) {
+		let newNode = Node(value: value, next: nil, previous: tail)
+		tail?.next = newNode
+		tail = newNode
+		if head == nil {
+			head = newNode
+		}
+	}
+}
--- BSVBattleRoyaleTests/BSVBattleRoyaleTests.swift
+++ BSVBattleRoyaleTests/BSVBattleRoyaleTests.swift
@@ -19,16 +19,42 @@ class BSVBattleRoyaleTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+	func testLinkedList() {
+		let list = LinkedList<Int>()
+		XCTAssertEqual(list.head?.value, nil)
+		XCTAssertEqual(list.tail?.value, nil)
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+		list.addToHead(value: 1)
+		XCTAssertEqual(list.head?.value, 1)
+		XCTAssertEqual(list.tail?.value, 1)
 
+		list.addToHead(value: 2)
+		XCTAssertEqual(list.head?.value, 2)
+		XCTAssertEqual(list.tail?.value, 1)
+
+		let tailList = LinkedList<Int>()
+		tailList.addToTail(value: 1)
+		XCTAssertEqual(tailList.head?.value, 1)
+		XCTAssertEqual(tailList.tail?.value, 1)
+
+		tailList.addToTail(value: 2)
+		XCTAssertEqual(tailList.head?.value, 1)
+		XCTAssertEqual(tailList.tail?.value, 2)
+
+		tailList.addToTail(value: 3)
+		XCTAssertEqual(tailList.head?.value, 1)
+		XCTAssertEqual(tailList.tail?.value, 3)
+
+		tailList.addToHead(value: 0)
+		XCTAssertEqual(tailList.head?.value, 0)
+		XCTAssertEqual(tailList.tail?.value, 3)
+
+		var node = tailList.head
+		var array = [Int]()
+		while let unwrap = node {
+			array.append(unwrap.value)
+			node = node?.next
+		}
+		XCTAssertEqual(array, [0, 1, 2, 3])
+	}
 }
