40e664590769c36a9cf857ee5e515a58ad85d791
Michael Redig <mredig@gmail.com>
2020-02-09 14:59:45 -0600

    upgraded connection to ssl - security!!

--- BSVBattleRoyale/Helpers/Constants.swift
+++ BSVBattleRoyale/Helpers/Constants.swift
@@ -15,6 +15,8 @@ let playerBitmask: UInt32 = 1 << 2
 
 //let backendDomain = "localhost:8000" //local
 //let backendDomain = "bsvbattle.redig.me:8040" //dev
-let backendDomain = "bsvbattle.redig.me:8000" //testflight
-let backendBaseURL = URL(string: "http://\(backendDomain)/")
-let backendWSURL = URL(string: "ws://\(backendDomain)")
+let backendDomain = "bsvbattle.redig.me" //testflight
+fileprivate let restProtocol = "https"
+fileprivate let wsProtocol = "wss"
+let backendBaseURL = URL(string: "\(restProtocol)://\(backendDomain)/")
+let backendWSURL = URL(string: "\(wsProtocol)://\(backendDomain)")
