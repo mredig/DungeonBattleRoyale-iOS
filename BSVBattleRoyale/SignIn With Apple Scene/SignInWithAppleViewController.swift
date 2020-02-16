1cfaec49e91d0ead3882d04987c9286353ec2b27
Michael Redig <mredig@gmail.com>
2020-02-16 17:44:43 -0600

    (nit) fixed signin vc to tabs instead of spaces

--- BSVBattleRoyale/SignIn With Apple Scene/SignInWithAppleViewController.swift
+++ BSVBattleRoyale/SignIn With Apple Scene/SignInWithAppleViewController.swift
@@ -11,22 +11,22 @@ import AuthenticationServices
 import NetworkHandler
 
 class SignInWithAppleViewController: UIViewController {
-    
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var password1TextField: UITextField!
-    @IBOutlet weak var password2TextField: UITextField!
-    @IBOutlet weak var registerLoginButton: UIButton!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var password1TextField: UITextField!
+	@IBOutlet weak var password2TextField: UITextField!
+	@IBOutlet weak var registerLoginButton: UIButton!
+	@IBOutlet weak var segmentedControl: UISegmentedControl!
 	@IBOutlet weak var collectionView: UICollectionView!
 
-    var signInType: SignInType = .login
-       
-       enum SignInType {
-           case register
-           case login
-       }
-       
-    let apiController = APIController()
+	var signInType: SignInType = .login
+
+	enum SignInType {
+		case register
+		case login
+	}
+
+	let apiController = APIController()
 	
 	var avatars: [AvatarSelectionContainer] = Avatar.allCases.map {
 		let container = AvatarSelectionContainer()
@@ -34,65 +34,65 @@ class SignInWithAppleViewController: UIViewController {
 		return container
 	}
 
-    
-    @IBAction func registerButtonTapped(_ sender: Any) {
-        
-        print("register button tapped")
-        
-        if segmentedControl.selectedSegmentIndex == 0 {
-            signInType = .register
-            registerUser()
-        } else {
-            signInType = .login
-            loginUser()
-        }
-    }
 
-    
-    @IBAction func segmentedControlChanged(_ sender: Any) {
-        
-        if segmentedControl.selectedSegmentIndex == 0 {
-            signInType = .register
-            UIView.transition(with: registerLoginButton, duration: 0.3, options: [.transitionCrossDissolve, .curveLinear], animations: {
-              self.registerLoginButton.setTitle("Register", for: .normal)
-              self.hideViewWithFade(self.password2TextField)
-            }, completion: nil)
-                        
+	@IBAction func registerButtonTapped(_ sender: Any) {
 
-        } else {
-            signInType = .login
-            UIView.transition(with: registerLoginButton, duration: 0.4, options: [.transitionCrossDissolve, .curveLinear], animations: {
-              self.registerLoginButton.setTitle("Login", for: .normal)
-              self.hideViewWithFade(self.password2TextField)
-            }, completion: nil)
+		print("register button tapped")
+
+		if segmentedControl.selectedSegmentIndex == 0 {
+			signInType = .register
+			registerUser()
+		} else {
+			signInType = .login
+			loginUser()
+		}
+	}
 
 
-        }
-    }
-    
-    // MARK: - UI Actions
+	@IBAction func segmentedControlChanged(_ sender: Any) {
 
-    @objc func signInButtonPressed() {
+		if segmentedControl.selectedSegmentIndex == 0 {
+			signInType = .register
+			UIView.transition(with: registerLoginButton, duration: 0.3, options: [.transitionCrossDissolve, .curveLinear], animations: {
+				self.registerLoginButton.setTitle("Register", for: .normal)
+				self.hideViewWithFade(self.password2TextField)
+			}, completion: nil)
 
-        // First you create an apple id provider request with the scope of full name and email
-        let request = ASAuthorizationAppleIDProvider().createRequest()
-        request.requestedScopes = [.fullName, .email]
 
-        // Instanstiate and configure the authorization controller
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-        authorizationController.presentationContextProvider = self
-        authorizationController.delegate = self
+		} else {
+			signInType = .login
+			UIView.transition(with: registerLoginButton, duration: 0.4, options: [.transitionCrossDissolve, .curveLinear], animations: {
+				self.registerLoginButton.setTitle("Login", for: .normal)
+				self.hideViewWithFade(self.password2TextField)
+			}, completion: nil)
 
-        // Perform the request
-        authorizationController.performRequests()
-    }
 
-    // MARK: - View Life Cycle
+		}
+	}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        setupAppleIDButton()
+	// MARK: - UI Actions
+
+	@objc func signInButtonPressed() {
+
+		// First you create an apple id provider request with the scope of full name and email
+		let request = ASAuthorizationAppleIDProvider().createRequest()
+		request.requestedScopes = [.fullName, .email]
+
+		// Instanstiate and configure the authorization controller
+		let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+		authorizationController.presentationContextProvider = self
+		authorizationController.delegate = self
+
+		// Perform the request
+		authorizationController.performRequests()
+	}
+
+	// MARK: - View Life Cycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		//        setupAppleIDButton()
 
 
 		registerLoginButton.layer.cornerRadius = 8
@@ -104,170 +104,170 @@ class SignInWithAppleViewController: UIViewController {
 		collectionView.dataSource = self
 
 		//FIXME: TEST ONLY
-//		usernameTextField.text = "ffff"
-//		password1TextField.text = "Aabc123!"
-//		segmentedControl.selectedSegmentIndex = 1
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
+		//		usernameTextField.text = "ffff"
+		//		password1TextField.text = "Aabc123!"
+		//		segmentedControl.selectedSegmentIndex = 1
+	}
 
-    // MARK: - View Setup
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+	}
 
-    func setupAppleIDButton() {
+	// MARK: - View Setup
 
-        // Instantiate the button with a type and style
-        let signInButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
+	func setupAppleIDButton() {
 
-        // Add an action to be called when tapping the button
-        signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
+		// Instantiate the button with a type and style
+		let signInButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
 
-        // Add button to view and setup constraints
-        signInButton.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(signInButton)
-        NSLayoutConstraint.activate([
-            signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            signInButton.widthAnchor.constraint(equalToConstant: 250),
-            signInButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
-    }
+		// Add an action to be called when tapping the button
+		signInButton.addTarget(self, action: #selector(signInButtonPressed), for: .touchUpInside)
 
-    // MARK: - Presentations
+		// Add button to view and setup constraints
+		signInButton.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(signInButton)
+		NSLayoutConstraint.activate([
+			signInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			signInButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+			signInButton.widthAnchor.constraint(equalToConstant: 250),
+			signInButton.heightAnchor.constraint(equalToConstant: 44)
+		])
+	}
 
-    func presentCouldNotAuthenticateAlert() {
-        // Present alert
-        
-    }
-    
-    // MARK: - Transitions
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        super.prepare(for: segue, sender: sender)
-        if let dest = segue.destination as? ViewController {
-            dest.apiController = apiController
-        }
-    }
-    
-    // smooth fade in/out for views
-    func hideViewWithFade(_ view: UIView) {
-        if view.isHidden {
-            view.alpha = 0.0
-        }
+	// MARK: - Presentations
 
-        view.isHidden = false
+	func presentCouldNotAuthenticateAlert() {
+		// Present alert
 
-        UIView.animate(withDuration: 0.3, delay: 0.0, options: .transitionCrossDissolve, animations: {
-            view.alpha = view.alpha == 1.0 ? 0.0 : 1.0
-        }, completion: { _ in
-            view.isHidden = !Bool(truncating: view.alpha as NSNumber)
-        })
-    }
+	}
 
-    // MARK: - Networking
-    
-    func registerUser() {
-        guard let username = usernameTextField.text,
-            let password = password1TextField.text,
-            let password2 = password2TextField.text,
-            password2 == password else { return }
-        
-        apiController.register(with: username, password: password) { (error) in
-            if let error = error {
-                if let terror = error as? NetworkError {
-                    switch terror {
-                    case .httpNon200StatusCode(code: _, data: let Data):
-                        let string = String(data: Data!, encoding: .utf8)
-                        print(string as Any)
-                    default:
-                        break
-                    }
-                }
-                NSLog("Error registering \(error)")
-                return
-            }
-            if self.apiController.token != nil {
-                self.startGame()
-            }
-        }
-    }
-    
-    
-    func startGame() {
+	// MARK: - Transitions
+
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		super.prepare(for: segue, sender: sender)
+		if let dest = segue.destination as? ViewController {
+			dest.apiController = apiController
+		}
+	}
+
+	// smooth fade in/out for views
+	func hideViewWithFade(_ view: UIView) {
+		if view.isHidden {
+			view.alpha = 0.0
+		}
+
+		view.isHidden = false
+
+		UIView.animate(withDuration: 0.3, delay: 0.0, options: .transitionCrossDissolve, animations: {
+			view.alpha = view.alpha == 1.0 ? 0.0 : 1.0
+		}, completion: { _ in
+			view.isHidden = !Bool(truncating: view.alpha as NSNumber)
+		})
+	}
+
+	// MARK: - Networking
+
+	func registerUser() {
+		guard let username = usernameTextField.text,
+			let password = password1TextField.text,
+			let password2 = password2TextField.text,
+			password2 == password else { return }
+
+		apiController.register(with: username, password: password) { (error) in
+			if let error = error {
+				if let terror = error as? NetworkError {
+					switch terror {
+					case .httpNon200StatusCode(code: _, data: let Data):
+						let string = String(data: Data!, encoding: .utf8)
+						print(string as Any)
+					default:
+						break
+					}
+				}
+				NSLog("Error registering \(error)")
+				return
+			}
+			if self.apiController.token != nil {
+				self.startGame()
+			}
+		}
+	}
+
+
+	func startGame() {
 		let selectedIndex = avatars.firstIndex(where: { $0.isSelected} ) ?? 0
 		let avatar = Avatar(rawValue: selectedIndex) ?? .yellowMonster
 		apiController.selectedAvatar = avatar
-        DispatchQueue.main.async {
-            self.performSegue(withIdentifier: "ShowSegueToMainStoryboard", sender: nil)
-        }
-    }
-    
-    
-    func loginUser() {
-        guard let username = usernameTextField.text,
-            let password = password1TextField.text else { return }
-        
-        apiController.login(with: username, password: password) { (error) in
-            if let error = error {
-                if let terror = error as? NetworkError {
-                    switch terror {
-                    case .httpNon200StatusCode(code: _, data: let Data):
-                        let string = String(data: Data!, encoding: .utf8)!
-                        print(string)
-                    default:
-                        break
-                    }
-                }
-                NSLog("Error logging in user \(error)")
-                return
-            }
-            if self.apiController.token != nil {
-                self.startGame()
-            }
-        }
-    }
+		DispatchQueue.main.async {
+			self.performSegue(withIdentifier: "ShowSegueToMainStoryboard", sender: nil)
+		}
+	}
 
-    func exchangeCode(_ code: String, handler: (String?, Error?) -> Void) {
-        // Call your backend to exchange an API token with the code.
-        print("Auth Code: \(code)")
-        performSegue(withIdentifier: "ShowSegueToMainStoryboard", sender: nil)
-    }
+
+	func loginUser() {
+		guard let username = usernameTextField.text,
+			let password = password1TextField.text else { return }
+
+		apiController.login(with: username, password: password) { (error) in
+			if let error = error {
+				if let terror = error as? NetworkError {
+					switch terror {
+					case .httpNon200StatusCode(code: _, data: let Data):
+						let string = String(data: Data!, encoding: .utf8)!
+						print(string)
+					default:
+						break
+					}
+				}
+				NSLog("Error logging in user \(error)")
+				return
+			}
+			if self.apiController.token != nil {
+				self.startGame()
+			}
+		}
+	}
+
+	func exchangeCode(_ code: String, handler: (String?, Error?) -> Void) {
+		// Call your backend to exchange an API token with the code.
+		print("Auth Code: \(code)")
+		performSegue(withIdentifier: "ShowSegueToMainStoryboard", sender: nil)
+	}
 }
 
 extension SignInWithAppleViewController: ASAuthorizationControllerPresentationContextProviding {
 
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return view.window!
-    }
+	func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+		return view.window!
+	}
 }
 
 extension SignInWithAppleViewController: ASAuthorizationControllerDelegate {
 
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        let authError = ASAuthorizationError(_nsError: error as NSError)
-        switch authError.code {
-        case .canceled:
-            break
-        default:
-            presentCouldNotAuthenticateAlert()
-        }
-    }
+	func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+		let authError = ASAuthorizationError(_nsError: error as NSError)
+		switch authError.code {
+		case .canceled:
+			break
+		default:
+			presentCouldNotAuthenticateAlert()
+		}
+	}
 
-    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            if let data = credential.authorizationCode, let code = String(data: data, encoding: .utf8) {
-                // Now send the 'code' to your backend to get an API token.
-                exchangeCode(code) { apiToken, error in
-                    // Handle response
-                    
-                }
-            } else {
-                // Handle missing authorization code ...
-                
-            }
-        }
-    }
+	func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+		if let credential = authorization.credential as? ASAuthorizationAppleIDCredential {
+			if let data = credential.authorizationCode, let code = String(data: data, encoding: .utf8) {
+				// Now send the 'code' to your backend to get an API token.
+				exchangeCode(code) { apiToken, error in
+					// Handle response
+
+				}
+			} else {
+				// Handle missing authorization code ...
+
+			}
+		}
+	}
 }
 
 extension SignInWithAppleViewController: UICollectionViewDataSource, UICollectionViewDelegate {
