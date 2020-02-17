8880947d50152b96ab77e4746aa41341a4fdb799
Michael Redig <mredig@gmail.com>
2020-02-16 18:28:26 -0600

    (refactor) renamed more elements from BSV to Dungeon

--- DungeonBattleRoyaleTests/DungeonBattleRoyaleTests.swift
+++ DungeonBattleRoyaleTests/DungeonBattleRoyaleTests.swift
@@ -7,9 +7,9 @@
 //
 
 import XCTest
-@testable import BSVBattleRoyale
+@testable import DungeonBattleRoyale
 
-class BSVBattleRoyaleTests: XCTestCase {
+class DungeonBattleRoyaleTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- DungeonBattleRoyaleUITests/DungeonBattleRoyaleUITests.swift
+++ DungeonBattleRoyaleUITests/DungeonBattleRoyaleUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class BSVBattleRoyaleUITests: XCTestCase {
+class DungeonBattleRoyaleUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
